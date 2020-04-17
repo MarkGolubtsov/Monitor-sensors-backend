3d8a45a53f62adc06262387d0ed885e5dd8aedc4
Mark <glmarkgl4@gmail.com>
2020-04-18 02:57:29 +0300

    Added default users for tests.

--- sql/generation-data.sql
+++ sql/generation-data.sql
@@ -18,3 +18,15 @@ INSERT INTO units (name)
 VALUES ('%');
 
 
+INSERT INTO monitor_sensors (name, model, range_from, range_to, type_id, unit_id, location, description)
+ VALUES ('Sensor_1','PC33-56',0,16,1,1,'Room1','Some input');
+
+INSERT INTO monitor_sensors (name, model, range_from, range_to, type_id, unit_id, location, description)
+ VALUES ('Sensor_2','EH-567',-25,25,2,2,'Room1','Some input');
+
+INSERT INTO monitor_sensors (name, model, range_from, range_to, type_id, unit_id, location, description)
+VALUES ('Sensor_3','PC33-56',-70,50,3,3,'Room2','Some input');
+
+INSERT INTO monitor_sensors (name, model, range_from, range_to, type_id, unit_id, location, description)
+VALUES ('Sensor_4','H94',0,100,4,4,'Room2','Some input');
+
---
+++ sql/generation-users.sql
@@ -0,0 +1,2 @@
+INSERT INTO users  (email,password,roles) VALUES ('viewer@email.com','viewer','ROLE_VIEWER');
+INSERT INTO users  (email,password,roles) VALUES ('admin@email.com','admin','ROLE_ADMIN');
